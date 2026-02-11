a540c4328b8a9759edcf80b232b76a188de86b38
Thomas Vermeer <THQgameworld@gmail.com>
2026-02-11 09:45:43 +0100

    commit main app page open soldatenpage test (works)
    de main app een scherm gemaakt met knoppen naar verschillende schermen dit was om te testen of de navigatie zou werken en dit klopt de soldatenscreen word correct geopend
    (commit was van gisteren maar was vergeten deze commit te sturen)

--- src/main/java/com/example/jtech_beroepsproduct/MainApp.java
+++ src/main/java/com/example/jtech_beroepsproduct/MainApp.java
@@ -1,4 +1,57 @@
 package com.example.jtech_beroepsproduct;
 
-public class MainApp {
+import com.example.jtech_beroepsproduct.screens.*;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
+
+public class MainApp extends Application {
+
+    @Override
+    public void start(Stage stage) {
+        // 1. De balk met knoppen
+        HBox navBar = new HBox(15);
+        navBar.getStyleClass().add("nav-bar");
+
+        Button btnSoldaten = new Button("Soldaten");
+        Button btnMateriaal = new Button("Materiaal");
+
+        // 2. Koppel de CSS klassen uit je styleguide
+        btnSoldaten.getStyleClass().add("nav-button");
+        btnMateriaal.getStyleClass().add("nav-button");
+
+        // 3. ACTIE: Open soldaten in een nieuw venster
+        btnSoldaten.setOnAction(e -> {
+            Stage venster = new Stage();
+            venster.setTitle("Soldaten Beheer");
+
+            // Maak een nieuwe scene voor dit specifieke venster
+            Scene soldatenScene = new Scene(new SoldatenScreen(), 800, 600);
+
+            // Gebruik het juiste relatieve pad naar je CSS
+            soldatenScene.getStylesheets().add(getClass().getResource("stylesheets/style.css").toExternalForm());
+
+            venster.setScene(soldatenScene);
+            venster.show();
+        });
+
+
+        navBar.getChildren().addAll(btnSoldaten, btnMateriaal);
+
+
+        Scene homeScene = new Scene(navBar, 400, 100);
+
+
+        homeScene.getStylesheets().add(getClass().getResource("/com/example/jtech_beroepsproduct/stylesheets/style.css").toExternalForm());
+
+        stage.setTitle("J-Tac Menu");
+        stage.setScene(homeScene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
 }
--- src/main/java/com/example/jtech_beroepsproduct/screens/SoldatenScreen.java
+++ src/main/java/com/example/jtech_beroepsproduct/screens/SoldatenScreen.java
@@ -1,4 +1,19 @@
 package com.example.jtech_beroepsproduct.screens;
 
-public class SoldatenScreen {
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+
+// Door 'extends VBox' te gebruiken, ziet Java dit als een Parent/Node
+public class SoldatenScreen extends VBox {
+
+    public SoldatenScreen() {
+        // Maak een simpel tekstje aan
+        Label tekst = new Label("soldatenscreen");
+
+        // Voeg de tekst toe aan dit scherm
+        this.getChildren().add(tekst);
+
+        // Gebruik de headline stijl uit je styleguide
+        tekst.getStyleClass().add("headline-1");
+    }
 }
