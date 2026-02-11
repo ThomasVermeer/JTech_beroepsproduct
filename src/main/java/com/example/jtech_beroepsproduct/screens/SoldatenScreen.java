e97912aa91b62805a7c5e817e0d6575d4b2aa548
Thomas Vermeer <THQgameworld@gmail.com>
2026-02-11 10:11:45 +0100

    soldatenscreen looks + commented code when soldaten model is complete
    het uiterlijk van de soldatenscreen gemaakt doormiddel van een tableview (aangeraden door ai) de code die nog uit ge comment is moet ik gebruiken wanneer de soldaten model is ingevuld (om warnings weg te werken) is niet verplicht maar voor netheid wel

--- src/main/java/com/example/jtech_beroepsproduct/screens/SoldatenScreen.java
+++ src/main/java/com/example/jtech_beroepsproduct/screens/SoldatenScreen.java
@@ -1,19 +1,67 @@
 package com.example.jtech_beroepsproduct.screens;
 
-import javafx.scene.control.Label;
+import javafx.geometry.Insets;
+import javafx.scene.control.*;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
-// Door 'extends VBox' te gebruiken, ziet Java dit als een Parent/Node
 public class SoldatenScreen extends VBox {
 
     public SoldatenScreen() {
-        // Maak een simpel tekstje aan
-        Label tekst = new Label("soldatenscreen");
+        //Instellingen voor de layout (padding en ruimte tussen elementen)
+        this.setPadding(new Insets(20));
+        this.setSpacing(20);
 
-        // Voeg de tekst toe aan dit scherm
-        this.getChildren().add(tekst);
+        // titel en headline 1 ophalen uit onze stylesheet
+        Label titel = new Label("Soldaten Beheer");
+        titel.getStyleClass().add("headline-1");
 
-        // Gebruik de headline stijl uit je styleguide
-        tekst.getStyleClass().add("headline-1");
+        // invoer velden maken met placeholder tekst
+        HBox inputBalk = new HBox(10);
+
+        TextField txtNummer = new TextField();
+        txtNummer.setPromptText("soldaat nummer");
+
+        TextField txtNaam = new TextField();
+        txtNaam.setPromptText("soldaat naam");
+
+        TextField txtRang = new TextField();
+        txtRang.setPromptText("soldaat rang");
+
+        DatePicker dateGeboorte = new DatePicker();
+        dateGeboorte.setPromptText("geboorte datum");
+
+        Button btnOpslaan = new Button("soldaat maken of bijwerken");
+        btnOpslaan.getStyleClass().add("action-button");
+
+        inputBalk.getChildren().addAll(txtNummer, txtNaam, txtRang, dateGeboorte, btnOpslaan);
+
+        // 4. De Tabel
+        TableView tableView = new TableView();
+
+        TableColumn colNummer = new TableColumn("soldaat nummer");
+        TableColumn colNaam = new TableColumn("soldaat naam");
+        TableColumn colRang = new TableColumn("soldaat rang");
+        TableColumn colGeboorte = new TableColumn("geboorte datum");
+        TableColumn colActies = new TableColumn("acties");
+
+
+// code hier onder gebruiken wanneer soldaten model gemaakt en ingevuld is (huidige code puur voor visueel dus
+// geen zorgen over de warnings)
+
+        //TableView<Soldaat> tableView = new TableView<>();
+        //TableColumn<Soldaat, String> colNummer = new TableColumn<>("soldaat nummer");
+        //TableColumn<Soldaat, String> colNaam = new TableColumn<>("soldaat naam");
+        //TableColumn<Soldaat, String> colRang = new TableColumn<>("soldaat rang");
+        //TableColumn<Soldaat, String> colGeboorte = new TableColumn<>("geboorte datum");
+
+
+        tableView.getColumns().addAll(colNummer, colNaam, colRang, colGeboorte, colActies);
+
+        //gewenste hoogte instellen van de tabel (kan nog aanpassen moet wel dan bij elke page het zelfde zijn
+        tableView.setPrefHeight(400);
+
+
+        this.getChildren().addAll(titel, inputBalk, tableView);
     }
 }
