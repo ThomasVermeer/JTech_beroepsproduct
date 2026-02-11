0f80ef11fd7ff2b6c2445b0145b25b032493061f
Thomas Vermeer <THQgameworld@gmail.com>
2026-02-11 10:20:21 +0100

    fill model Soldaat
    de getters en setters in de model plaatsen om eigenlijk aan te geven : dit bestaat hallo jij weet nu wat een soldaat is

--- src/main/java/com/example/jtech_beroepsproduct/model/Soldaat.java
+++ src/main/java/com/example/jtech_beroepsproduct/model/Soldaat.java
@@ -1,4 +1,25 @@
 package com.example.jtech_beroepsproduct.model;
 
+import java.time.LocalDate;
+
 public class Soldaat {
+    // de variabelen aangeven (private voor veiligheid)
+    private String nummer;
+    private String naam;
+    private String rang;
+    private LocalDate geboorteDatum;
+
+    // constructor om een soldaat aan te kunnen maken
+    public Soldaat(String nummer, String naam, String rang, LocalDate geboorteDatum) {
+        this.nummer = nummer;
+        this.naam = naam;
+        this.rang = rang;
+        this.geboorteDatum = geboorteDatum;
+    }
+
+    // getters die de tableview nodig heeft om de info uit te halen
+    public String getNummer() { return nummer; }
+    public String getNaam() { return naam; }
+    public String getRang() { return rang; }
+    public LocalDate getGeboorteDatum() { return geboorteDatum; }
 }
