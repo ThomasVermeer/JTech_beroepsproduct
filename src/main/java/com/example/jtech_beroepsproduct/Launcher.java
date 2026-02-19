4ea2a287d522b00485cb896e5e54a2c9ea90c2ea
Thomas Vermeer <THQgameworld@gmail.com>
2026-02-19 11:20:31 +0100

    correction version 17

--- src/main/java/com/example/jtech_beroepsproduct/Launcher.java
+++ src/main/java/com/example/jtech_beroepsproduct/Launcher.java
@@ -4,7 +4,7 @@ import javafx.application.Application;
 
 public class Launcher {
     public static void main(String[] args) {
-        // we veranderen HelloApplication naar MainApp zodat het veilig verwijderd kan worden
+        // we veranderen HelloApplication naar MainApp zodat het veilig verwijderd kan wordenn
         Application.launch(MainApp.class, args);
     }
 }
